185469a2db8d0190dac3fb0dbf46b31281092004
Pete MC-Mods <mcmods.pete@gmail.com>
2025-01-11 23:18:28 +0100

    add config option to display countdown to horde night (issue #4), change color of messages to dark red

--- src/main/java/net/petemc/undeadnights/world/spawner/UndeadSpawner.java
+++ src/main/java/net/petemc/undeadnights/world/spawner/UndeadSpawner.java
@@ -7,8 +7,9 @@ import net.minecraft.registry.tag.FluidTags;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
+import net.minecraft.text.Style;
 import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
+import net.minecraft.util.Colors;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.random.Random;
@@ -146,6 +147,15 @@ public class UndeadSpawner implements Spawner {
                 daysCounter = daysCounter - 1;
                 serverState.daysCounter = daysCounter;
                 serverState.markDirty();
+                if ((daysCounter > 0) && (UndeadNightsConfig.INSTANCE.sendHordeNightsCountdownMessage)) {
+                    for (ServerPlayerEntity player : world.getPlayers()) {
+                        if (daysCounter > 1) {
+                            player.sendMessage(Text.literal(daysCounter + " nights remaining until the next Night of the Undead!"));
+                        } else {
+                            player.sendMessage(Text.literal("This is the last night before the next Night of the Undead!"));
+                        }
+                    }
+                }
                 if (UndeadNightsConfig.INSTANCE.printDebugMessages) {
                     UndeadNights.LOGGER.info("Night is coming, NormalizedTimeOfDay: {}, TimeOfDay: {} DaysCounter: {}", normalizedTimeOfDay, world.getTimeOfDay(), daysCounter);
                 }
@@ -204,7 +214,7 @@ public class UndeadSpawner implements Spawner {
                     serverState.spawnZombies = true;
                     serverState.markDirty();
                     for (ServerPlayerEntity player : world.getPlayers()) {
-                        player.sendMessage(Text.literal("The sun is starting to set and you feel uneasy about the coming night...").formatted(Formatting.RED));
+                        player.sendMessage(Text.literal("The sun is starting to set and you feel uneasy about the coming night...").setStyle(Style.EMPTY.withColor(Colors.RED)));
                     }
                     if (UndeadNightsConfig.INSTANCE.printDebugMessages) {
                         UndeadNights.LOGGER.info("The coming night is a Horde Night, HordeNight: {}", UndeadNights.hordeNight);
@@ -265,7 +275,7 @@ public class UndeadSpawner implements Spawner {
                         if (UndeadNights.globalSpawnCounter < UndeadNightsConfig.INSTANCE.hordeZombiesSpawnCap) {
                             if (i == 0) {
                                 player.getWorld().playSound(null, player.getX(), player.getY(), player.getZ(), UndeadNightsSounds.HORDE_SCREAM, SoundCategory.HOSTILE, 4.0F, 1);
-                                player.sendMessage(Text.literal("A horde has spawned!").formatted(Formatting.RED));
+                                player.sendMessage(Text.literal("A horde has spawned!").setStyle(Style.EMPTY.withColor(Colors.RED)));
                                 if (UndeadNightsConfig.INSTANCE.printDebugMessages) {
                                     UndeadNights.LOGGER.info("A Horde has spanned!");
                                 }
