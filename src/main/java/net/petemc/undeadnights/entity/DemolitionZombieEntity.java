f2f33071a862752f5b3be2bb6dcd7ce84d79886f
Pete MC-Mods <mcmods.pete@gmail.com>
2025-03-21 13:34:46 +0100

    added spawn eggs for horde_zombie, elite_zombie and demolition_zombie, added /undeadnights command (to spawn_horde, get status, print_config, remove_mobs), for mobs from other mods, the FOLLOW_RANGE will be extended 128, added drops for elite_zombie, made loading horde config more robust

--- src/main/java/net/petemc/undeadnights/entity/DemolitionZombieEntity.java
+++ src/main/java/net/petemc/undeadnights/entity/DemolitionZombieEntity.java
@@ -10,7 +10,6 @@ import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.mob.ZombieEntity;
 import net.minecraft.entity.passive.IronGolemEntity;
 import net.minecraft.entity.passive.MerchantEntity;
-import net.minecraft.entity.passive.TurtleEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -42,10 +41,10 @@ public class DemolitionZombieEntity extends ZombieEntity {
         return HostileEntity.createHostileAttributes()
                 .add(EntityAttributes.MAX_HEALTH, 40.0)       // default 20.0
                 .add(EntityAttributes.FOLLOW_RANGE, 128.0)    // default 35.0
-                .add(EntityAttributes.MOVEMENT_SPEED, 0.30f)  // default 0.23000000417232513
+                .add(EntityAttributes.MOVEMENT_SPEED, 0.30)   // default 0.23000000417232513
                 .add(EntityAttributes.ATTACK_DAMAGE, 5.0)     // default 3.0
                 .add(EntityAttributes.ARMOR, 4.0)             // default 2.0
-                .add(EntityAttributes.SPAWN_REINFORCEMENTS, 0.0f);
+                .add(EntityAttributes.SPAWN_REINFORCEMENTS, 0.0);
     }
 
     @Override
@@ -58,9 +57,8 @@ public class DemolitionZombieEntity extends ZombieEntity {
         this.goalSelector.add(7, new WanderAroundFarGoal(this, 1.0));
         this.targetSelector.add(1, new RevengeGoal(this).setGroupRevenge(HordeZombieEntity.class));
         this.targetSelector.add(2, new ActiveTargetGoal<>(this, PlayerEntity.class, false, false));
-        this.targetSelector.add(3, new ActiveTargetGoal<>(this, MerchantEntity.class, false));
+        this.targetSelector.add(3, new ActiveTargetGoal<>(this, MerchantEntity.class, true));
         this.targetSelector.add(3, new ActiveTargetGoal<>(this, IronGolemEntity.class, true));
-        this.targetSelector.add(5, new ActiveTargetGoal<>(this, TurtleEntity.class, 10, true, false, TurtleEntity.BABY_TURTLE_ON_LAND_FILTER));
     }
 
     @Nullable
---
+++ src/main/java/net/petemc/undeadnights/mixin/ServerEntityManagerMixin.java
@@ -0,0 +1,22 @@
+package net.petemc.undeadnights.mixin;
+
+import net.minecraft.server.world.ServerEntityManager;
+import net.minecraft.world.entity.EntityLike;
+import net.petemc.undeadnights.UndeadNights;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ServerEntityManager.class)
+public class ServerEntityManagerMixin
+{
+    @Inject(method = "unload(Lnet/minecraft/world/entity/EntityLike;)V", at = @At("HEAD"))
+    public void unload(EntityLike entityLike, CallbackInfo ci)
+    {
+        if (UndeadNights.serverState.spawnedHordeMobs.contains(entityLike.getUuid())) {
+            UndeadNights.globalSpawnCounter--;
+            UndeadNights.LOGGER.info("UNLOAD GlobalSpawnCount: {} {} {}", UndeadNights.globalSpawnCounter, "UNLOADED_TO_CHUNK", entityLike.getUuid());
+        }
+    }
+}
