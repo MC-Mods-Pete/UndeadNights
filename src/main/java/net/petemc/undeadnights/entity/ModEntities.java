202d346b822e58cbf513e299655a1ed6d2dbc3fb
Pete MC-Mods <mcmods.pete@gmail.com>
2025-05-01 23:36:19 +0200

    changes for 1.2.0 update

--- src/main/java/net/petemc/undeadnights/entity/ModEntities.java
+++ src/main/java/net/petemc/undeadnights/entity/ModEntities.java
@@ -11,13 +11,24 @@ public class ModEntities {
     public static final EntityType<HordeZombieEntity> HORDE_ZOMBIE = Registry.register(Registries.ENTITY_TYPE,
             Identifier.of(UndeadNights.MOD_ID, "horde_zombie"),
             EntityType.Builder.create(HordeZombieEntity::new, SpawnGroup.MONSTER)
-                    .dimensions(0.6f, 1.95f).build(Identifier.of(UndeadNights.MOD_ID, "horde_zombie").toString()));
+            .dimensions(0.6f, 1.95f)
+            .build(Identifier.of(UndeadNights.MOD_ID, "horde_zombie").toString()));
     public static final EntityType<DemolitionZombieEntity> DEMOLITION_ZOMBIE = Registry.register(Registries.ENTITY_TYPE,
             Identifier.of(UndeadNights.MOD_ID, "demolition_zombie"),
             EntityType.Builder.create(DemolitionZombieEntity::new, SpawnGroup.MONSTER)
-                    .dimensions(0.6f, 1.95f).build(Identifier.of(UndeadNights.MOD_ID, "demolition_zombie").toString()));
+            .dimensions(0.6f, 1.95f)
+            .build(Identifier.of(UndeadNights.MOD_ID, "demolition_zombie").toString()));
     public static final EntityType<EliteZombieEntity> ELITE_ZOMBIE = Registry.register(Registries.ENTITY_TYPE,
             Identifier.of(UndeadNights.MOD_ID, "elite_zombie"),
             EntityType.Builder.create(EliteZombieEntity::new, SpawnGroup.MONSTER)
-                    .dimensions(0.6f, 1.95f).build(Identifier.of(UndeadNights.MOD_ID, "elite_zombie").toString()));
+            .dimensions(0.6f, 1.95f)
+            .build(Identifier.of(UndeadNights.MOD_ID, "elite_zombie").toString()));
+    public static final EntityType<DemolitionZombieProjectileEntity> TNT_PROJECTILE = Registry.register(Registries.ENTITY_TYPE,
+            Identifier.of(UndeadNights.MOD_ID, "tnt_projectile"),
+            EntityType.Builder.<DemolitionZombieProjectileEntity>create(DemolitionZombieProjectileEntity::new, SpawnGroup.MISC)
+            //.setShouldReceiveVelocityUpdates(true)
+            .maxTrackingRange(64)
+            .trackingTickInterval(1)
+            .dimensions(0.5f, 0.5f)
+            .build(Identifier.of(UndeadNights.MOD_ID, "tnt_projectile").toString()));
 }
