5acf574f1b17135200c297eb94b70990de58deed
Pete MC-Mods <mcmods.pete@gmail.com>
2025-05-01 22:13:16 +0200

    set permission for undeadnights command

--- src/main/java/net/petemc/undeadnights/command/SetDefaultHordeCommand.java
+++ src/main/java/net/petemc/undeadnights/command/SetDefaultHordeCommand.java
@@ -17,6 +17,7 @@ import java.util.Objects;
 public class SetDefaultHordeCommand {
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher, CommandRegistryAccess commandRegistryAccess, CommandManager.RegistrationEnvironment registrationEnvironment) {
         dispatcher.register(CommandManager.literal("undeadnights")
+                .requires(source -> source.hasPermissionLevel(2))
                 .then(CommandManager.literal("default_horde")
                 .then(CommandManager.argument("hordeId", IntegerArgumentType.integer(0))
                 .executes((command) -> {
