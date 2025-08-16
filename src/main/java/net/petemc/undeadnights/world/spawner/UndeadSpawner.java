c34f5d6e38191fe3b656fec114ed52c39635b561
Pete MC-Mods <mcmods.pete@gmail.com>
2025-08-16 22:39:44 +0200

    fix issue where spawning a horde can get stuck in a loop (variant 1), other small fix

--- src/main/java/net/petemc/undeadnights/world/spawner/UndeadSpawner.java
+++ src/main/java/net/petemc/undeadnights/world/spawner/UndeadSpawner.java
@@ -288,8 +288,8 @@ public class UndeadSpawner implements CustomSpawner {
                                 randomValue = randomSource.nextIntBetweenInclusive(1, 100);
                                 if (spawnHordeMob(level, randomSource, pos, player, (randomValue > (100 - mobSpawnData.chance())) ? mobSpawnData : HordeConfig.getDefaultHordeMob()) == 0) {
                                     spawnCounter++;
-                                    waveMobCounter++;
                                 }
+                                waveMobCounter++;
                                 if (waveMobCounter >= HordeConfig.getMaxWaveSize()) {
                                     d = 0;
                                     break;
@@ -305,8 +305,8 @@ public class UndeadSpawner implements CustomSpawner {
                     } else {
                         if (spawnHordeMob(level, randomSource, pos, player, HordeConfig.getDefaultHordeMob()) == 0) {
                             spawnCounter++;
-                            waveMobCounter++;
                         }
+                        waveMobCounter++;
                     }
                 }
             }
