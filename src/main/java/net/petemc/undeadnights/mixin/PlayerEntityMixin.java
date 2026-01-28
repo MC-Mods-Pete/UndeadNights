3d5d113b7d190c97b1d96cee115e657cb8852c4e
Pete MC-Mods <mcmods.pete@gmail.com>
2026-01-28 23:38:26 +0100

    fix: possible crash when trying to sleep in a bed

--- src/main/java/net/petemc/undeadnights/mixin/PlayerEntityMixin.java
+++ src/main/java/net/petemc/undeadnights/mixin/PlayerEntityMixin.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(PlayerEntity.SleepFailureReason.class)
 public class PlayerEntityMixin
 {
-    @Inject(method = "getMessage", at = @At(value = "RETURN"), cancellable = true)
+    @Inject(method = "message", at = @At(value = "RETURN"), cancellable = true)
     public void getMessage (CallbackInfoReturnable<Text> cir)
     {
         if (UndeadNights.serverState.getHordeNight() && MainConfig.getHordeNightsDisableSleeping()) {
