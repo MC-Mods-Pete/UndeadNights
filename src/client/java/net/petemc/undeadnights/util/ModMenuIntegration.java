633c65c2216070296f2a600a11c2799ea2550eee
Pete MC-Mods <mcmods.pete@gmail.com>
2026-03-29 23:17:04 +0200

    fix problem with mod menu integration

--- src/client/java/net/petemc/undeadnights/util/ModMenuIntegration.java
+++ src/client/java/net/petemc/undeadnights/util/ModMenuIntegration.java
@@ -14,6 +14,6 @@ public class ModMenuIntegration implements ModMenuApi
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory()
     {
-        return parent -> AutoConfig.getConfigScreen(MainConfig.class, parent).get();
+        return parent -> AutoConfigClient.getConfigScreen(MainConfig.class, parent).get();
     }
 }
