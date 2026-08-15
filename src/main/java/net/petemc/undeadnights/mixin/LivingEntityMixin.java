97b35198803b370d7fbf0ef4b84a81b8551ba232
Pete MC-Mods <mcmods.pete@gmail.com>
2026-08-15 11:25:16 +0200

    fix problem with "Zombies Climbing each other" not working

--- src/main/java/net/petemc/undeadnights/mixin/LivingEntityMixin.java
+++ src/main/java/net/petemc/undeadnights/mixin/LivingEntityMixin.java
@@ -1,20 +1,29 @@
 package net.petemc.undeadnights.mixin;
 
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.util.Mth;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.monster.zombie.Zombie;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
 import net.petemc.undeadnights.UndeadNights;
+import net.petemc.undeadnights.casts.BlockBreakingZombie;
 import net.petemc.undeadnights.casts.UndeadNightsExtendedPlayer;
 import net.petemc.undeadnights.effect.ModEffects;
+import net.petemc.undeadnights.util.ModTags;
 import net.petemc.undeadnights.util.RandomExtention;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Comparator;
+import java.util.List;
 
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin
@@ -44,6 +53,50 @@ public class LivingEntityMixin
                             }
                             player.addEffect(new MobEffectInstance(ModEffects.LURE_HORDE, UndeadNights.difficultyConfig.getCurrentDifficultyLevel().getDifficultySettingsLureEffect().getDurationForLureHordeEffect() * 20, 0));
                         }
+                    }
+                }
+            }
+        }
+    }
+
+
+    @Inject(method = "tick", at = @At("TAIL"))
+    public void tick(CallbackInfo ci) {
+        Entity entity = (Entity) (Object) this;
+
+        if (entity.getType().builtInRegistryHolder().is(ModTags.EntityTypes.HORDE_MOBS)) {
+            if (entity instanceof BlockBreakingZombie blockBreakingZombie) {
+                if (blockBreakingZombie.isBreakingBlock()) {
+                    return;
+                }
+            }
+
+            if (UndeadNights.difficultyConfig.getCurrentDifficultyLevel().getDifficultySettingsHordeMobs().isHordeMobsCanClimbEachOther()) {
+                final Vec3 entityPosition = entity.position();
+                final AABB entitySearchArea = new AABB(entityPosition, entityPosition).inflate(0.45 / 2d);
+                List<Entity> sortedEntityList = entity.level().getEntitiesOfClass(Entity.class, entitySearchArea, entityTagCheck ->
+                                entityTagCheck.getType().builtInRegistryHolder().is(ModTags.EntityTypes.HORDE_MOBS))
+                        .stream().sorted(Comparator.comparingDouble(entityDistSort -> entityDistSort.distanceToSqr(entityPosition))).toList();
+
+                for (Entity hordeMobIterator : sortedEntityList) {
+                    if (entity.getX() != hordeMobIterator.getX()) {
+                        double randomValue = Math.random();
+                        if (randomValue > 0.16D) {
+                            randomValue = 0.16D;
+                        }
+                        Vec3 entityVec3 = new Vec3(
+                                (entity.getDeltaMovement().x + (randomValue / 20.0D) * Mth.nextInt(entity.getRandom(), -1, 1)),
+                                randomValue,
+                                (entity.getDeltaMovement().z + (randomValue / 20.0D) * Mth.nextInt(entity.getRandom(), -1, 1)));
+
+                        entity.setDeltaMovement(entityVec3);
+                        if (entity instanceof LivingEntity livingEntity) {
+                            if (livingEntity.isBaby()) {
+                                entity.setDeltaMovement(entityVec3.add(0, randomValue, 0));
+                            }
+                            livingEntity.addEffect(new MobEffectInstance(MobEffects.SLOW_FALLING, 5, 0, false, false));
+                        }
+                        entity.fallDistance = 0;
                     }
                 }
             }
